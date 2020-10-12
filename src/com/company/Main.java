393c1fd603b5395eb9715c7be1734b1449431490
panda-ak <raketaidin@gmail.com>
2020-10-12 21:41:52 +0300

    panda delo

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        repatPrinting();
+        repatPrinting();
+        repatPrinting();
+
+    }
+
+    public static void repatPrinting() {
+
+        System.out.println("java");
+        System.out.println("geektech");
+        System.out.println("hello");
+
+
+
+    }
+}
